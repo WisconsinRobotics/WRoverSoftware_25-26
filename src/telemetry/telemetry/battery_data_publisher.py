f4f4b6a0f27f1e2bcabb6aa94b720077104ef797
Christopher Teggatz <categgatz@wisc.edu>
2025-10-16 19:05:53 -0500

    added starter code for the battery publisher

--- src/telemetry/telemetry/battery_data_publisher.py
+++ src/telemetry/telemetry/battery_data_publisher.py
@@ -1,3 +1,26 @@
+# Import the relevent node packages
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+"""
+Class responsible for getting, processing, and sending out battery data to subscribers.
+This will use serial port via a USB in order to communicate with the:q
+
+"""
+class BatteryDataPublisher(Node):
+    #Basic construstructor
+    def __init__(self):
+        pass
+
+    def timer_callback(self):
+        pass
+
+
+
+
 def main():
     print('Hi from telemetry.')
 
