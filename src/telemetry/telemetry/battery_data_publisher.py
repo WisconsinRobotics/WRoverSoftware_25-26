b6fa1f7f1fc464bccc3ab7ac0502969488e31965
Christopher Teggatz <categgatz@wisc.edu>
2025-10-25 19:40:33 -0500

    added multithreading to node

--- src/telemetry/telemetry/battery_data_publisher.py
+++ src/telemetry/telemetry/battery_data_publisher.py
@@ -11,28 +11,6 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 
-"""GLOBAL DATA"""
-data = 0
-dataLock = threading.Lock()
-
-serialEventLoop = threading.Event()
-
-"""
-This will go through the serial data and try to read shit
-"""
-def serial_worker():
-    # While the loop is allowed to do stuff, try to read the data on the serial bus
-    while(serialEventLoop.is_set()):
-        pass
-        # inside of here, if we get a full set of data, write to data
-        # use a buffer here. Write to the buffer each time we actually have data
-        # if we find an escape sequence in the buffer, add the data, remove that part from the buffer
-        # should probably use a bytearray()
-    pass
-
-
-
-
 """
 Class responsible for getting, processing, and sending out battery data to subscribers.
 This will use serial port via a USB in order to communicate with the ESP32
@@ -46,6 +24,19 @@ class BatteryDataPublisher(Node):
         # Init node class stuff
         super().__init__("BatteryDataPublisher")
         
+        # Make the worker thread that gets telemetry data
+        self.get_logger().info("starting up worker")
+        self.serial_data = ""
+        self.data = ""
+        self.serial_data_lock = threading.Lock()
+        self.serial_event_loop = threading.Event()
+#        self.serial_event_loop.set()
+
+        self.serial_worker = threading.Thread(target = self.get_serial_worker)
+        self.serial_worker.start()
+
+
+
         # Make publisher, this will publish strings of JSON files
         self.publisher_ = self.create_publisher(String, "battery_telemetry", 10)
 
@@ -54,42 +45,72 @@ class BatteryDataPublisher(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         pass
 
-
     """
     Ros event loop that will send out data to publishers    
     """
     def timer_callback(self):
         # Create the Message to send out   
         msg = String()    
-        msg.data = "Hello World"
+
+        if(self.serial_data_lock.acquire(blocking = False)):
+            self.data = self.serial_data
+            self.serial_data_lock.release()
+
+        msg.data = "data : " + self.data
 
         # Publish out the message
         self.publisher_.publish(msg)
         self.get_logger().info("Publishing: " + msg.data)
         pass
 
-
-
     """
-    Requests data from the serial bus and converts that into useful data.
-    sends out a request 
+    Responsible for cleaning up processes on kill of the node
     """
-    def request_data(self):
-        # We will use the PySerial Framework to read data. getting information is going
-        # to be a different story
+    def on_cleanup(self):
+        #self.get_logger().info("shutting down serial worker")
+        print("shutting down serial worker")
+        # stop tell the event to stop reading
+        self.serial_event_loop.set()
+        self.serial_worker.join()
+        print("workers joined")
+    
+    """
+    This will go through the serial data and try to read shit
+    """
+    def get_serial_worker(self):
+        self.get_logger().info("Starting serial worker")
+        # While the loop is allowed to do stuff, try to read the data on the serial bus
+        counter = 0
+
+        while not self.serial_event_loop.is_set():
+            # Write the data if we get any
+            counter += 1 
+            
+            if(counter % 10 == 0):
+                with self.serial_data_lock:
+                    self.serial_data = f"{counter}"
+            pass
+            # inside of here, if we get a full set of data, write to data
+            # use a buffer here. Write to the buffer each time we actually have data
+            # if we find an escape sequence in the buffer, add the data, remove that part from the buffer
+            # should probably use a bytearray()
         pass
+
 
 def main(args = None):
     # ROS STARTUP
     rclpy.init(args = args)
-
     battery_data_publisher = BatteryDataPublisher()
 
-    rclpy.spin(battery_data_publisher)
-
-    #kill the node i guess
-    battery_datda_publisher.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(battery_data_publisher)
+    except KeyboardInterrupt:
+        print("accepted keyboard interupt")
+    finally:
+        battery_data_publisher.on_cleanup()
+        #kill the node i guess
+        battery_data_publisher.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
