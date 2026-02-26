90f2e38635b16886aec9358b91d74470ca4841c9
Balabalu-VE <nicolasdittmarg1@gmail.com>
2026-02-26 16:08:42 -0600

    missed a merge conflict

--- src/aruco_part/setup.py
+++ src/aruco_part/setup.py
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-from setuptools import find_packages, setup
-
-package_name = 'aruco_part'
-
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Rodrigo',
-    maintainer_email='rochisigo@gmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-            'driving_logic = aruco_part.driving_logic:main',
-            'fake_tag = aruco_part.fake_tag:main',
-            'fake_sensors = aruco_part.fake_sensors:main',
-            'path_visualizer = aruco_part.path_visualizer:main',
-            'camera_info = aruco_part.camera_info:main',
-            'finding_tag = aruco_part.finding_tag:main',
-            'combined = aruco_part.combined:main',
-        ],
-    },
-)
-=======
 from setuptools import find_packages, setup
 
 package_name = 'aruco_part'
@@ -74,4 +36,3 @@ setup(
         ],
     },
 )
->>>>>>> 212c4f5df5ddd4a9021fce6f299db118412428f4
