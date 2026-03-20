69a2bb8d2bc91e6385e2d43c84ac2f41688e0557
Christopher Teggatz <categgatzjr@gmail.com>
2026-03-19 22:40:20 -0500

    gitignore

--- build/wr_path_planning/prefix_override/sitecustomize.py
+++ build/wr_path_planning/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/teggatz/Documents/code/WRoverSoftware_25-26/install/wr_path_planning'
+    sys.prefix = sys.exec_prefix = '/home/teggatz/Documents/WiscRobo/WRoverSoftware_25-26/install/wr_path_planning'
