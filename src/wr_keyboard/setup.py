20dabb799561e08aad025b98cf1b93a40b790c23
Andrue Dettman <addettman@wisc.edu>
2026-03-26 18:52:46 -0500

    l

--- src/wr_keyboard/setup.py
+++ src/wr_keyboard/setup.py
@@ -24,7 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-		'keyboard_detection = wr_keyboard.keyboard_detection:main'
+            'keyboard_test = wr_keyboard.keyboard_test:main'
         ],
     },
 )
