a316398c75ccf9e9e0c92e9a7e9d2713d2ae8e17
aarav <2005.aarav.agrawal@gmail.com>
2026-05-19 21:10:22 -0500

    put object detection into ros2 node

---
+++ src/navigation/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'resource'), glob('resource/*.blob')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aarav',
+    maintainer_email='2005.aarav.agrawal@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'object_detection = navigation.object_detection:main'
+        ],
+    },
+)
