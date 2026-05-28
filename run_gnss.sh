bae22d7813d3ed99abd1e71d55b6c53ac531459f
David Wang <wang3458@wisc.edu>
2026-05-27 22:15:01 -0500

    Fixed launch scripts

--- run_gnss.sh
+++ run_gnss.sh
@@ -4,5 +4,3 @@ ros2 run ublox_dgnss_node ublox_dgnss_node --ros-args -r __ns:=/rover1 -p device
 ros2 run ublox_dgnss_node ublox_dgnss_node --ros-args -r __ns:=/rover2 -p device_serial_string:="'2'" &
 ros2 run wr_imu_compass compass &
 ros2 run gps_tools single_heading &
-ros2 run master_finder master_finder &
-ros2 run wr_path_planning path_planner --ros-args -r lidar_path:="~/motel.laz" &
--- run_state_machine_base_station.sh
+++ run_state_machine_base_station.sh
@@ -3,5 +3,6 @@
 LIDAR_PATH="$1"
 
 ros2 run master_finder master_finder &
-ros2 run wr_path_planning path_planner --ros-args -r lidar_path:="$LIDAR_PATH" &
+ros2 run wr_path_planning path_planner --ros-args -p lidar_file:="$LIDAR_PATH" &
+sleep 10
 ros2 run state_machine state_machine_controller
