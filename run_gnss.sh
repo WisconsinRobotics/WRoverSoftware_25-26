131971be25fcbe8f9057efdd29f348f63346687c
David Wang <wang3458@wisc.edu>
2026-05-29 23:02:46 -0500

    Modified run_gnss.sh

--- run_gnss.sh
+++ run_gnss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source install/setup.bash
-ros2 run ublox_dgnss_node ublox_dgnss_node --ros-args -r __ns:=/rover1 -p device_serial_string:="'1'" &
-# ros2 run ublox_dgnss_node ublox_dgnss_node --ros-args -r __ns:=/rover2 -p device_serial_string:="'2'" &
+ros2 run ublox_dgnss_node ublox_dgnss_node --ros-args -r __ns:=/rover1 -p DEVICE_SERIAL_STRING:="'3'" &
+ros2 run ublox_dgnss_node ublox_dgnss_node --ros-args -r __ns:=/rover2 -p DEVICE_SERIAL_STRING:="'4'" &
 ros2 run wr_imu_compass compass &
-ros2 run gps_tools single_heading &
+ros2 run gps_tools heading &
